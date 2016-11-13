cef758fe0c95a790778b36dec2f52269e541ce1d
hnuzhoulin <hnuzhoulin@gmail.com>
2016-11-14 00:14:15 +0800

    feat:using avmo
    using avmo to download VENUs

--- spider.py
+++ spider.py
@@ -28,17 +28,18 @@ def control_link(link):
 def download_img(download_list,path):
     for img_url in download_list:
         try:
+            print "downloading:"+img_url
             request = urllib2.Request(img_url)
-            request.add_header('Accept-Encoding','gzip, deflate, sdch')
+            request.add_header('Accept-Encoding','gzip, deflate, sdch, br')
             request.add_header('Accept-Language','zh-CN,zh;q=0.8,en;q=0.6,bs;q=0.4,zh-TW;q=0.2')
             request.add_header('User-Agent','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
             request.add_header('Accept','text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-            request.add_header('Referer','http://rb.arzon.jp/item_1340270.html')
-            request.add_header('Cookie','PHPSESSID=fdld9oe628f0k0qiuibvleaq65')
-            request.add_header('Host','rb.arzon.jp')
-            f_img = open(os.path.join(path, img_url.split('/')[-1]),'wb')
-            f_img.write(urllib2.urlopen(request).read())
-            f_img.close()
+            request.add_header('Referer','https://avmo.pw/cn')
+            request.add_header('Cookie','AD_enterTime=1479049398; AD_clic_j_POPUNDER=2; AD_juic_j_M_728x90=1; AD_javu_j_P_728x90=1; AD_exoc_j_POPUNDER=1; AD_juic_j_L_728x90=4; AD_adst_j_POPUNDER=2; _ga=GA1.2.1043844509.1479049395; AD_exoc_j_L_728x90=1')
+            img_name = img_url.split('/')[-1]
+            with open(os.path.join(path, img_name),'wb') as f_img:
+                f_img.write(urllib2.urlopen(request).read())
+            time.sleep(1)
         except Exception as e:
             print("Save file error:",img_url)
         # filename = img_url.split('/')[-1]
@@ -49,14 +50,14 @@ def download_img(download_list,path):
 
 def download_html(url):
     request = urllib2.Request(url)
-    request.add_header('Accept-Encoding', 'gzip, deflate, sdch')
+    request.add_header('Accept-Encoding', 'gzip, deflate, sdch, br')
     request.add_header('Accept-Language', 'zh-CN,zh;q=0.8,en;q=0.6,bs;q=0.4,zh-TW;q=0.2')
     request.add_header('User-Agent',
                        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36')
     request.add_header('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8')
-    request.add_header('Referer', 'http://rb.arzon.jp/item_1340270.html')
-    request.add_header('Cookie', 'PHPSESSID=fdld9oe628f0k0qiuibvleaq65')
-    request.add_header('Host', 'rb.arzon.jp')
+    request.add_header('Referer', 'https://avmo.pw/cn')
+    request.add_header('Cookie',
+                       'AD_enterTime=1479049398; AD_clic_j_POPUNDER=2; AD_juic_j_M_728x90=1; AD_javu_j_P_728x90=1; AD_exoc_j_POPUNDER=1; AD_juic_j_L_728x90=4; AD_adst_j_POPUNDER=2; _ga=GA1.2.1043844509.1479049395; AD_exoc_j_L_728x90=1')
     response = urllib2.urlopen(request)
     if response.code == 200:
         content = response.read()
@@ -65,33 +66,53 @@ def download_html(url):
             content = zlib.decompress(content, 16 + zlib.MAX_WBITS)
         return content
     else:
-        print "Response code is ",response.code,",,url is ",url
+        print "Response code is ",response.code,",url is ",url
 
-def SpiderMain(root_url,path):
-    search_con = download_html(root_url)
-    search_soup = BeautifulSoup(search_con, "html.parser")
-    item_con = search_soup.find('li',class_="topContents")
-    item_url = item_con.find(href=re.compile(r'/item_[0-9]*.html')).attrs['href']
+def SpiderMain(root_url,itemRange):
+    root_dir = os.getcwd()
+    for i in itemRange:
+        i = "%03d"%i
 
-    # spider items
-    if item_url is None:
-        print "Error,not found item in ",root_url
-    else:
+        path = os.path.join(root_dir,"VENU-"+i)
+        if not os.path.exists(path):
+            os.mkdir(path)
+
+        item_search_url = root_url+"VENU-"+i
+        search_con = download_html(item_search_url)
+        search_soup = BeautifulSoup(search_con, "html.parser")
+        item_con = search_soup.find_all('a',class_="movie-box")
+        # print len(item_con)
+        if len(item_con) == 1:
+            item_url = item_con[0].attrs['href']
+            # print item_url
+        elif len(item_con) == 0:
+            print "item VENU-"+i+" does nit exist"
+            continue
+        else:
+            print "Search VENU-"+i+" have multi-result,next"
+            continue
+
+        # spider items
         item_con = download_html(control_link(item_url))
         item_soup = BeautifulSoup(item_con, "html.parser")
         download_list = []
+
         ## spider fengmian
-        fengmian_con = item_soup.find(id="productExtra")
-        img = fengmian_con.find('a')
-        download_list.append(control_link(img.attrs['href']))
+        fengmian_con = item_soup.find('a',class_="bigImage")
+        fengmian_url = fengmian_con.attrs['href']
+        download_list.append(control_link(fengmian_url))
 
         ##spider info
-        identifer_soup = item_soup.find(id="productIdentifier")
+        identifer_soup = item_soup.find('div',class_="col-md-3 info")
+        info = identifer_soup.get_text()
+        avatar_link = item_soup.find_all('a',class_="avatar-box")
+        if len(avatar_link) == 1:
+            info = info + u"演员作品列表:\n" +avatar_link[0].attrs['href']
         with open(os.path.join(path,'info.txt'),'w') as f:
-            f.write(identifer_soup.get_text())
+            f.write(info)
 
         ##sipder screenshot
-        screnshot_soup = item_soup.find(id="sample-image-block")
+        screnshot_soup = item_soup.find(id="sample-waterfall")
         if screnshot_soup is not None:
             for img in screnshot_soup.find_all('a'):
                 download_list.append(control_link(img.attrs['href']))
@@ -100,22 +121,12 @@ def SpiderMain(root_url,path):
         else:
             print "no screenshot for item:",item_url
         # print item_con.find(href='/item_1340698.html')
+        # print download_list
+        time.sleep(2)
 
 
 
 if __name__ == '__main__':
-    root_dir = os.getcwd()
-    # root_url = "http://rb.arzon.jp"
     root_url = "https://avmo.pw/cn/search/"
-    search_para = 'itemlist_b.html?&q=VENU-'
-    for i in xrange(001,661):
-        i = "%03d"%i
-        try:
-            path = os.path.join(root_dir,"VENU-"+str(i))
-            if not os.path.exists(path):
-                os.mkdir(path)
-            SpiderMain(root_url+"/"+search_para+str(i),path)
-            print "Success ----VENU-",i
-            time.sleep(1)
-        except Exception as e:
-            print "Error----VENU-",i,"   error code is:",e
+    itemRange = xrange(1,2)
+    SpiderMain(root_url,itemRange)
